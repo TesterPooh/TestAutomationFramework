405befb39d7e45050eced3b8d1ac7bc4d39585ce
TesterPooh <poonamkolhe3@gmail.com>
2026-06-09 13:57:26 -0400

    Update LoginTest.java

--- src/test/java/com/ui/test/LoginTest.java
+++ src/test/java/com/ui/test/LoginTest.java
@@ -41,36 +41,31 @@ public void logintest(User user) {
 	   
 }
 	
-///*
-// * @Test ( description =
-// * "Verifies valid user is able to login into application", groups = {"e2e",
-// * "sanity"},dataProviderClass =
-// * com.ui.dataproviders.LoginDataProvider.class,dataProvider =
-// * "LoginTestCSVDataProvider" ) public void loginCSVtest(User user) {
-// * 
-// * 
-// * assertEquals (homepage.gotoLoginPage().doLoginWith(user.getEmailaddress(),
-// * user.getPassword()).getUserName(), "Pooh nemade");
-// * 
-// * 
-// * }
-// * 
-// * 
-// * 
-// * 
-// * @Test ( description =
-// * "Verifies valid user is able to login into application", groups = {"e2e",
-// * "sanity"},dataProviderClass =
-// * com.ui.dataproviders.LoginDataProvider.class,dataProvider =
-// * "LoginTestxlsDataProvider", retryAnalyzer =
-// * com.ui.listeners.MyRetryAnalyzer.class ) public void loginxlstest(User user)
-// * {
-// * 
-// * 
-// * 
-// * assertEquals (homepage.gotoLoginPage().doLoginWith(user.getEmailaddress(),
-// * user.getPassword()).getUserName(), "Pooh nemade");
-// * 
-// * 
-// * }
+
+ @Test ( description =
+"Verifies valid user is able to login into application", groups = {"e2e",
+ "sanity"},dataProviderClass =
+ com.ui.dataproviders.LoginDataProvider.class,dataProvider =
+"LoginTestCSVDataProvider" ) public void loginCSVtest(User user) {
+
+
+ assertEquals (homepage.gotoLoginPage().doLoginWith(user.getEmailaddress(),
+user.getPassword()).getUserName(), "Pooh nemade");
+
+ }
+ 
+
+
+
+ @Test ( description =
+ "Verifies valid user is able to login into application", groups = {"e2e",
+"sanity"},dataProviderClass =
+com.ui.dataproviders.LoginDataProvider.class,dataProvider =
+ "LoginTestxlsDataProvider", retryAnalyzer =
+ com.ui.listeners.MyRetryAnalyzer.class ) public void loginxlstest(User user)
+ {  assertEquals (homepage.gotoLoginPage().doLoginWith(user.getEmailaddress(),
+ user.getPassword()).getUserName(), "Pooh nemade");
+ 
+
+ }
 	} 
